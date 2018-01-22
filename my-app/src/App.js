95983ce4794811c1758d7f151807c3072031ce8d
馒头 <15958022388@163.com>
2018-01-22 17:15:43 +0800

    添加组件生命周期方法

---
+++ my-app/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  componentWillMount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+
+  componentDidMount() {//初始化数据，组件装配后立即调用
+    
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {//可以进行DOM操作
+
+  }
+
+  componentWillUnmount() {//在组件被卸载和销毁前调用
+
+  }
+}
+
+export default App;
